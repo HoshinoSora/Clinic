93d928fd6a33be4a78f56c425e9849cbeaaf882b
HoshinoSora <hoshinosora114@gmail.com>
2018-03-13 21:47:00 +0630

    change menus to menuitems; and add events

--- src/com/clinic/controller/AdminController.java
+++ src/com/clinic/controller/AdminController.java
@@ -1,54 +1,118 @@
 package com.clinic.controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 
 public class AdminController {
 
-    @FXML
-    private VBox adVbAddMember;
+	@FXML
+	private MenuItem addMember;
 
-    @FXML
-    private VBox adVbViewDoctor;
+	@FXML
+	private MenuItem changePassword;
 
-    @FXML
-    private VBox adVbViewReceptionist;
+	@FXML
+	private MenuItem logout;
 
-    @FXML
-    private VBox adVbViewPatient;
+	@FXML
+	private MenuItem viewDoctor;
 
-    @FXML
-    private VBox adChangePassword;
+	@FXML
+	private MenuItem viewReception;
 
-    @FXML
-    void AdAddMember(ActionEvent event) {
-    	System.out.println("blah");
-    }
+	@FXML
+	private MenuItem viewPatient;
 
-    @FXML
-    void AdChangePassword(ActionEvent event) {
+	@FXML
+	private VBox adVbAddDoctor;
 
-    }
+	@FXML
+	private VBox adVbViewDoctor;
 
-    @FXML
-    void AdLogout(ActionEvent event) {
+	@FXML
+	private VBox adVbViewReceptionist;
 
-    }
+	@FXML
+	private VBox adVbViewPatient;
 
-    @FXML
-    void AdViewDoctor(ActionEvent event) {
+	@FXML
+	private VBox adChangePassword;
 
-    }
+	@FXML
+	void EditMenu() {
+		addMember.setOnAction(new EventHandler<ActionEvent>() {
 
-    @FXML
-    void AdViewPatient(ActionEvent event) {
+			@Override
+			public void handle(ActionEvent event) {
+				adVbAddDoctor.visibleProperty().set(true);
+				adVbViewDoctor.visibleProperty().set(false);
+				adVbViewPatient.visibleProperty().set(false);
+				adVbViewReceptionist.visibleProperty().set(false);
+				adChangePassword.visibleProperty().set(false);
+			}
+		});
+		
+		changePassword.setOnAction(new EventHandler<ActionEvent>() {
 
-    }
+			@Override
+			public void handle(ActionEvent event) {
+				adVbAddDoctor.visibleProperty().set(false);
+				adVbViewDoctor.visibleProperty().set(false);
+				adVbViewPatient.visibleProperty().set(false);
+				adVbViewReceptionist.visibleProperty().set(false);
+				adChangePassword.visibleProperty().set(true);
+			}
+		});
+		
+		logout.setOnAction(new EventHandler<ActionEvent>() {
 
-    @FXML
-    void AdViewReception(ActionEvent event) {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("blah");
+			}
+		});
+	}
 
-    }
+	
+	@FXML
+	void ViewMenu(ActionEvent event) {
+		viewDoctor.setOnAction(new EventHandler<ActionEvent>() {
 
+			@Override
+			public void handle(ActionEvent event) {
+				adVbAddDoctor.visibleProperty().set(false);
+				adVbViewDoctor.visibleProperty().set(true);
+				adVbViewPatient.visibleProperty().set(false);
+				adVbViewReceptionist.visibleProperty().set(false);
+				adChangePassword.visibleProperty().set(false);
+			}
+		});
+		
+		viewPatient.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				adVbAddDoctor.visibleProperty().set(false);
+				adVbViewDoctor.visibleProperty().set(false);
+				adVbViewPatient.visibleProperty().set(true);
+				adVbViewReceptionist.visibleProperty().set(false);
+				adChangePassword.visibleProperty().set(false);
+			}
+		});
+		
+		viewReception.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				adVbAddDoctor.visibleProperty().set(false);
+				adVbViewDoctor.visibleProperty().set(false);
+				adVbViewPatient.visibleProperty().set(false);
+				adVbViewReceptionist.visibleProperty().set(true);
+				adChangePassword.visibleProperty().set(false);
+			}
+		});
+	}
 }
