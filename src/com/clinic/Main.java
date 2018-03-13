814ed283c84ff37647338c60b22f11e0d9a62fcc
ThuRein <thuyeiwin@gmail.com>
2018-03-13 10:19:13 +0630

    LoginDesign

--- src/com/clinic/Main.java
+++ src/com/clinic/Main.java
@@ -1,18 +1,22 @@
 package com.clinic;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root=FXMLLoader.load(getClass().getResource("view/Loginform.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
